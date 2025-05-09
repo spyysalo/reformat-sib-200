ba5d753ce5b63c71dfc357cd27cfb1cac8b3e26b
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2025-05-09 17:00:56 +0300

    Add support for Finnish

--- reformat_sib_200.py
+++ reformat_sib_200.py
@@ -15,8 +15,36 @@ def argparser():
     return ap
 
 
+# Translations for category names
+CATEGORY_MAPS = {
+    'fin_Latn': {
+        'entertainment': 'viihde',
+        'geography': 'maantiede',
+        'health': 'terveys',
+        'politics': 'politiikka',
+        'science/technology': 'tiede/teknologia',
+        'sports': 'urheilu',
+        'travel': 'matkailu',
+    },
+    'eng_Latn': {
+        'entertainment': 'entertainment',
+        'geography': 'geography',
+        'health': 'health',
+        'politics': 'politics',
+        'science/technology': 'science/technology',
+        'sports': 'sports',
+        'travel': 'travel',
+    }
+}
+
+
 def main(argv):
     args = argparser().parse_args(argv[1:])
+
+    if args.lang_script not in CATEGORY_MAPS:
+        print(f'No translations for {args.lang_script}', file=sys.stderr)
+        return 1
+    category_map = CATEGORY_MAPS[args.lang_script]
 
     source = load_dataset(args.source, args.lang_script)
 
@@ -25,6 +53,11 @@ def main(argv):
         source['validation'],
         source['test'],
     ])
+
+    def translate_category(e):
+        e["category"] = category_map[e["category"]]
+        return e
+    combined = combined.map(translate_category)
 
     categories = sorted(set(combined['category']))
     combined = combined.add_column('choices', [categories] * len(combined))
